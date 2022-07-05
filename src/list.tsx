e7488588addf880c00597a66e42cff0c05c4ef2f
sayho <fishcreek@naver.com>
2022-07-05 19:24:42 +0900

    initial)

---
+++ src/list.tsx
@@ -0,0 +1,26 @@
+import {ActionPanel, Detail, List, Action} from '@raycast/api'
+
+export default function Command() {
+    return (
+        <List>
+            <List.Item
+                icon="list-icon.png"
+                title="Greeting"
+                actions={
+                    <ActionPanel>
+                        <Action.Push title="Show Details" target={<Detail markdown="# Hey! 👋" />} />
+                    </ActionPanel>
+                }
+            />
+            <List.Item
+                icon="list-icon.png"
+                title="psyho"
+                actions={
+                    <ActionPanel>
+                        <Action.Push title="Show Details" target={<Detail markdown="# https://psyho.pw 👋" />} />
+                    </ActionPanel>
+                }
+            />
+        </List>
+    )
+}
---
+++ src/shareSecretAction.tsx
@@ -0,0 +1,42 @@
+import {Toast, showToast, Clipboard, Icon, Action} from '@raycast/api'
+import got from 'got'
+
+export function ShareSecretAction() {
+    async function handleSubmit(values: {secret: string; expireViews: number; expireDays: number}) {
+        if (!values.secret) {
+            showToast({
+                style: Toast.Style.Failure,
+                title: 'Secret is required',
+            })
+            return
+        }
+
+        const toast = await showToast({
+            style: Toast.Style.Animated,
+            title: 'Sharing secret',
+        })
+
+        try {
+            const {body} = await got.post('https://api.doppler.com/v1/share/secrets/plain', {
+                json: {
+                    secret: values.secret,
+                    expire_views: values.expireViews,
+                    expire_days: values.expireDays,
+                },
+                responseType: 'json',
+            })
+
+            await Clipboard.copy((body as any).authenticated_url)
+
+            toast.style = Toast.Style.Success
+            toast.title = 'Shared secret'
+            toast.message = 'Copied link to clipboard'
+        } catch (error) {
+            toast.style = Toast.Style.Failure
+            toast.title = 'Failed sharing secret'
+            toast.message = String(error)
+        }
+    }
+
+    return <Action.SubmitForm icon={Icon.Upload} title="Share Secret" onSubmit={handleSubmit} />
+}
